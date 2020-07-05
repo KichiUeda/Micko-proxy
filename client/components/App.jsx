94a6beb8c7bb32f2b95f4f4c14385689f019a936
nightlaro <mickocabacungan@gmail.com>
2020-07-04 21:01:18 -0700

    setting up styled component, some progress on actual template, dependencies added

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Navigation from "./Navigation.jsx";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <div>
+      
+    </div>
+  }
+}
---
+++ client/components/Navigation.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+
+const NavContainer = styled.div`
+  
+`
+export default Navigation;
