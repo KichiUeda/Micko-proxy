c52fee156b8c57e0403194a138415c8a0409af84
nightlaro <mickocabacungan@gmail.com>
2020-07-21 19:30:27 -0700

    fixed requested change

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config({
+  path: path.resolve(__dirname, '../.env');
+});
 const express = require('express');
 
 const app = express();
